d70015300d6e181c472d402d9b972ea7bd1f9659
waquer <waquer@hotmail.com>
2018-04-05 09:18:27 +0800

    add RegistryTool

---
+++ My Little Tools 3/RegistryTool.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+
+namespace MyLittleTools3
+{
+    class RegistryTool
+    {
+        RegistryKey key = Registry.LocalMachine.CreateSubKey("Software\\MyLittleTools");
+
+
+        public void SetValue(string name, string value)
+        {
+            key.SetValue(name, value, RegistryValueKind.String);
+            key.Close();
+        }
+
+        public string GetValue(string name)
+        {
+            string value = key.GetValue(name).ToString();
+            key.Close();
+            return value;
+        }
+
+    }
+}
